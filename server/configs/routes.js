2f1319f05d83175c2d42daf74282c4893d322664
humphreeze2003 <humphreyshazam66@gmail.com>
2025-11-13 14:26:39 +0300

    adjusted adit item route 3

--- server/configs/routes.js
+++ server/configs/routes.js
@@ -735,12 +735,12 @@ router.patch(`/edit_item` , memuploader.single('image') ,  async function(req , 
                     const readstream = Readable.from(upload.buffer);
                     readstream.pipe(uploadstream);
         
-                    readstream.on('finish' , function(){
+                    uploadstream.on('finish' , function(){
                            resolve(uploadstream.id);
                     })
         
         
-                    readstream.on('error' , function(err){
+                    uploadstream.on('error' , function(err){
                           reject(err);
                     })
                 })
